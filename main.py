b7a411cf5cc6fee4d4a24b19497905b218cdabca
asaf129@gmail.com <asaf129@gmail.com>
2020-01-14 17:04:21 +0200

    with main and print array

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("hey python")
+def print_arry(*arr):
+    for x in range(len(arr)): 
+        print(arr[x])
 
+def main():
+    print("python main function")
+    a = [1, 2, 3, 4, 5] 
+    print_arry(a)
+
+
+if __name__ == '__main__':
+    main()
